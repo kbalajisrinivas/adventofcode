3c16db4de6cfbe016ced25d7411ae22931ab6221
Balaji Kumar <bkumar@ezesoft.com>
2020-04-25 19:36:50 -0400

    day 14 problem. not the correct solution

--- src/day14.js
+++ src/day14.js
@@ -1,6 +1,7 @@
 
 const util = require('./utils');
 var all_elements = {};
+let basic_composition = {};
 function readInput() {
     const input = util.readFile('day14.txt');
     for (let i = 0; i < input.length; i++) {
@@ -9,8 +10,10 @@ function readInput() {
         let input_elements = get_composition_of_combination(input_line[0]);
         all_elements[output_element.elementName] = {
             quantity: parseInt(output_element.quantity, 10),
-            composition: input_elements
+            composition: input_elements,
+            reserves: 0
         }
+
     }
     console.log(input);
 }
@@ -20,7 +23,8 @@ function get_element_details(element) {
     let result = element.split(" ");
     return {
         quantity: parseInt(result[0], 10),
-        elementName: result[1]
+        elementName: result[1],
+        reserves: 0
     }
 }
 
@@ -39,48 +43,44 @@ function calculateRequiredOre() {
     let fuel_composition = all_elements["FUEL"].composition;
     let elements = [];
     for (let i = 0; i < fuel_composition.length; i++) {
-        let composition = calculateComposition(fuel_composition[i]);
-        elements.push(...composition);
-    }
-    // now group over all the elements and see how much ORE is needed
-    let basic_composition = {};
-    for (let i = 0; i < elements.length; i++) {
-        if (basic_composition[elements[i].elementName]) {
-            basic_composition[elements[i].elementName] += elements[i].quantity;
-        } else {
-            basic_composition[elements[i].elementName] = elements[i].quantity;
-        }
+        let composition = calculateComposition(fuel_composition[i], 1);
+        // elements.push(...composition);
     }
 
     let total = 0;
     for (key in basic_composition) {
-        let currentQuantity = basic_composition[key];
-        let composition_quantity = all_elements[key].quantity;
+        let currentQuantity = Math.ceil(basic_composition[key]);
+        let composition_quantity = all_elements[key].quantity
         let oreQuantity = all_elements[key].composition[0].quantity;
-        total += (Math.ceil(currentQuantity / composition_quantity)) * oreQuantity;
+        total += Math.ceil(currentQuantity / composition_quantity) * oreQuantity;
     }
     console.log(total);
 }
 
-function calculateComposition(element) {
+function calculateComposition(element, quantity) {
     let element_info = all_elements[element.elementName];
-    let isBasic = checkIfItisBasicElement(element.elementName);
-    let all_elements_info = [];
-    if (isBasic) {
-        all_elements_info.push({
-            quantity: element.quantity,
-            elementName: element.elementName
-        });
-        return all_elements_info;
-    }
-
     for (let i = 0; i < element_info.composition.length; i++) {
-        let calculated_composition = calculateComposition(element_info.composition[i]);
-        const proportion = Math.ceil(parseInt(element.quantity) / parseInt(element_info.quantity));
-        element.basic = multiplier(proportion, calculated_composition);
-        all_elements_info.push(...element.basic);
+        console.log(`element: ${element.elementName}, quantity: ${quantity}, elementInfoQuantity: ${element_info.quantity}`);
+        let quant1 = (element.quantity * quantity) - element.reserves;
+        const proportion = Math.ceil(quant1/ element_info.quantity);
+        console.log(`quant1: ${quant1}, element: ${element.elementName}, proportion: ${proportion}`);
+        let residual = proportion -(quant1/ element_info.quantity);
+        element.reserves = (element.quantity * quantity) % element_info.quantity;
+        let isBasic = checkIfItisBasicElement(element.elementName);
+        if (isBasic) {
+            console.log(`quant1: ${quant1}, element: ${element.elementName}, quantity: ${element.quantity * quantity}`)
+            console.log("=================");
+            if (basic_composition[element.elementName]) {
+                basic_composition[element.elementName] += element.quantity * quantity;
+            } else {
+                basic_composition[element.elementName] = element.quantity * quantity;
+            }
+            return;
+        } else {
+            calculateComposition(element_info.composition[i], proportion);
+        }
     }
-    return all_elements_info;
+    // return all_elements_info;
 }
 
 
@@ -89,13 +89,6 @@ function checkIfItisBasicElement(element) {
     return current_element.composition.some(e => e.elementName === "ORE");
 }
 
-function multiplier(quantity, composition) {
-    let composition_sum = [];
-    for (let i = 0; i < composition.length; i++) {
-        composition[i].quantity = parseInt(composition[i].quantity, 10) * parseInt(quantity, 10);
-    }
-    return composition;
-}
 var input = readInput();
 var result = calculateRequiredOre();
 
---
+++ src/testjs.js
@@ -0,0 +1,14 @@
+var all_tiles = {};
+// var currentTile = {
+//     counter: 0
+// };
+getProgramState();
+function getProgramState() {
+    console.log(all_tiles);
+}
+
+// var x={};
+// test();
+// function test(){
+//     console.log(x);
+// }
